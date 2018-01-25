b8d857f90434354fa59d508a406defe72c5e0f7c
Qiaosen Huang <joesonw@gmail.com>
2018-01-25 16:42:35 +0800

    fix @Component

--- src/injections/Component.ts
+++ src/injections/Component.ts
@@ -1,7 +1,4 @@
 import Container from './Container';
-import AutoWireMeta from './AutoWireMeta';
-import Metadata from '../utils/Metadata';
-import { resolve } from 'path';
 
 export default function Component(provider?: Symbol | string) {
     return (target: any) => {
--- src/injections/Configuration.ts
+++ src/injections/Configuration.ts
@@ -1,19 +1,21 @@
 import Metadata from '../utils/Metadata';
 import Container from './Container';
 
-function Configuration(target: any): any {
-    Container.provide(target);
-    const instance = Container.get(target);
-    const property  = Container.getMeta(target).property;
-    for (const key in property) {
-        const meta = property[key].find(meta => meta.provider === Symbol.for('t2ee:core:bean-provider'));
-        if (meta) {
-            const value = instance[key]();
-            Container.inject(meta.type, value)
-            Container.inject(meta.functionType, value)
-            Container.inject(key, value)
-        }
-    }
+function Configuration() {
+    return (target: any): any => {
+    	Container.provide(target);
+		const instance = Container.get(target);
+		const property  = Container.getMeta(target).property;
+		for (const key in property) {
+			const meta = property[key].find(meta => meta.provider === Symbol.for('t2ee:core:bean-provider'));
+			if (meta) {
+				const value = instance[key]();
+				Container.inject(meta.type, value)
+				Container.inject(meta.functionType, value)
+				Container.inject(key, value)
+			}
+		}
+	};
 }
 
 export default Configuration;
